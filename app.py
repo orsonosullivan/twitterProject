5cd5d769fb08d42608cb123eb4b6bd055ac1701b
orsonosullivan <orosulli@tcd.ie>
2024-08-07 23:41:34 +0100

    basic flask app

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 
-app = Flasl(__name__)
+app = Flask(__name__)
 
 if __name__ == "__main__":
     app.run()
+
+@app.route("/")
+def home():
+    return "Welcome to my Reassessment!"
