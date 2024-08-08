b91f4ae3c56f94e425febf9e83e2bb695e0bb514
orsonosullivan <orosulli@tcd.ie>
2024-08-08 23:14:20 +0100

    added env variables to twitterAPI.py from .env using os and initialized OAUth

--- twitterAPI.py
+++ twitterAPI.py
@@ -8,15 +8,12 @@ load_dot_env()
 
 app = Flask(__name__)
 
-#client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
+app.config["TWITTER_CLIENT_ID"] = os.getenv("TWITTER_CLIENT_ID")
+app.config["TWITTER_CLIENT_SECRET"] = os.getenv("TWITTER_CLIENT_SECRET")
+app.config["TWITTER_DOMAIN"] = os.getenv("TWITTER_DOMAIN")
+app.config["TWITTER_CALLBACK_URL"] = os.getenv("TWITTER_CALLBACK_URL")
 
-#query = 'dublin'
-
-#response = client.search_recent_tweets(query=query, max_results=10)
-
-#tweet_texts = [tweet.text for tweet in response.data]
-
-#print(tweet_texts)
+oauth = OAuth(app)
 
 #search function using tweepy 
 def process_query(query):
