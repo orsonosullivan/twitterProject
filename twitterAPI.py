3ca439dff003b21485f1aa1bfb3772171142df9f
orsonosullivan <orosulli@tcd.ie>
2024-08-08 21:17:45 +0100

    bug fixes and added search features in function

--- twitterAPI.py
+++ twitterAPI.py
@@ -1,32 +1,36 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 import tweepy 
 import config
 
 app = Flask(__name__)
 
-client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
+#client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
 
-query = 'dublin'
+#query = 'dublin'
 
-response = client.search_recent_tweets(query=query, max_results=10)
+#response = client.search_recent_tweets(query=query, max_results=10)
 
-tweet_texts = [tweet.text for tweet in response.data]
+#tweet_texts = [tweet.text for tweet in response.data]
 
-print(tweet_texts)
+#print(tweet_texts)
 
+#search function using tweepy 
+def process_query(query):
+    client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
+    response = client.search_recent_tweets(query=query, max_results=10)
+    tweet_texts = [tweet.text for tweet in response.data]
 
-
+#landing page 
 @app.route("/")
 def home():
     return render_template("welcomepage.html")
 
-
-@approute('/search', methods=['POST'])
+#search function
+@app.route('/search', methods=['POST'])
 def search():
-    searchTerm = request.form['query']
+    query = request.form['query']
     result = process_query(query)
-    return render_template('result.html', query=query,result=result)
-
+    return render_template('resultpage.html', query=query,result=result)
 
 if __name__ == "__main__":
     app.run()
